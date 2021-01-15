f891f3f2326b423da93a0a33a290750bb03530e3
Vasudev Kulkarni <vdk5384@gmail.com>
2021-01-15 15:24:45 +0530

    test

---
+++ Assets/Scripts/EnemyTankFIre.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankFIre : MonoBehaviour
+{
+    private Rigidbody shellRb;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Fire()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TankAI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAI : MonoBehaviour
+{
+    Animator anim;
+    public GameObject player;
+    public GameObject bullet;
+    public GameObject turret;
+    public Rigidbody shellRb;
+    public Transform m_FireTransform;
+    public float m_MinLaunchForce = 15f;
+
+    private float m_CurrentLaunchForce;
+
+    private void OnEnable()
+    {
+        m_CurrentLaunchForce = m_MinLaunchForce;
+ //     m_AimSlider.value = m_MinLaunchForce;
+    }
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+
+    void Fire()
+    {
+     // GameObject b = Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+
+        Rigidbody shellInstance = Instantiate(shellRb, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+     // b.GetComponent<Rigidbody>().AddForce(turret.transform.position);
+    }
+
+    public void StopFiring()
+    {
+        CancelInvoke("Fire"); //alternates
+    }
+
+    public void StartFiring()
+    {
+        InvokeRepeating("Fire", 0.5f, 0.5f);
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("distance", Vector3.Distance(transform.position, player.transform.position));
+    }
+
+}
